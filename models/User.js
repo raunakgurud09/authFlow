9dfe2ed9c987b5c19e8116dc6b796c83051a5e8b
Raunak <raunakgurud2002@gmail.com>
2022-07-31 19:16:35 +0530

    added user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { User } = require("../configs/RoleList");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide name"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+  role: {
+    User: {
+      type: String,
+      default: "User",
+    },
+    SuperAdmin: SUPERADMIN,
+    Admin: ADMIN,
+  },
+  refreshToken: [String],
+});
+
+module.exports = mongoose.model("User", userSchema);
