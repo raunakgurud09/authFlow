86ecd74176fb6b2d347e925e013a773bc2c01ead
Raunak <raunakgurud2002@gmail.com>
2022-07-31 19:55:52 +0530

    register user controller 1

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,37 @@ const bcrypt = require("bcryptjs");
 const { StatusCodes } = require("http-status-codes");
 
 const register = async (req, res) => {
-  res.send("register");
+  const { name, email, password } = req.body;
+  if ((!email, !password)) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "Email and Password are required" });
+  }
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    res.status(StatusCodes.CONFLICT).json({ message: "Email already exist" });
+  }
+
+  const verificationToken = await bcrypt.hash(password, 10);
+
+  try {
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role,
+      verificationToken,
+    });
+
+    res
+      .status(StatusCodes.CREATED)
+      .json({ success: `New user ${user} created!` });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
 };
 
 const login = async (req, res) => {
