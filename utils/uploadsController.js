1e5f80d9984b907ff1859c76e9f951d67264cca8
Raunak <raunakgurud2002@gmail.com>
2022-08-02 08:03:20 +0530

    basic starter line added

---
+++ utils/uploadsController.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const fs = require("fs");
+
+const uploadUserAvatarImageLocal = async (req, res) => {
+  console.log("uploadUserAvatarImageLocal");
+};
+
+module.exports = {
+  uploadUserAvatarImageLocal,
+};
