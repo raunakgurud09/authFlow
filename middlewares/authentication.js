d690eddc7f7134d822c016d89b918e54eb328d3a
Raunak <raunakgurud2002@gmail.com>
2022-08-28 21:53:29 +0530

    authUser in progress

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,5 +1,6 @@
 const { signedCookies } = require("cookie-parser");
 const { StatusCodes } = require("http-status-codes");
+const { isTokenValid, attachCookiesToResponse } = require("../utils/jwt");
 
 
 const authenticateUser = async (req, res, next) => {
@@ -10,10 +11,29 @@ const authenticateUser = async (req, res, next) => {
 };
 
 const authUser = async (req, res, next) => {
-    console.log(req.Cookie)
+    // console.log(req.cookies.accessToken)
+    const { refreshToken, accessToken } = req.cookies;
+
+    try {
+        if (accessToken) {
+            const payload = isTokenValid(accessToken)
+            req.user = payload.user
+            next();
+        }
+
+        const payload = isTokenValid(refreshToken);
+
+
+        attachCookiesToResponse({req,user:payload.user,refreshToken})
+
+        req.user = payload.user
+        next();
+
+    } catch (error) {
+        console.log(error)
+    }
+
     next();
-    // const { accessToken, refreshToken } = req.Cookie;
-    // console.log(accessToken, refreshToken)
 }
 
 
--- utils/jwt.js
+++ utils/jwt.js
@@ -10,8 +10,9 @@ const createJWT = ({ payload }) => {
 }
 
 const isTokenValid = (token) => {
-    jwt.verify(token, process.env.JWT_SECRET)
+    return jwt.verify(token, process.env.JWT_SECRET)
 }
+
 
 const attachCookiesToResponse = ({ res, user, refreshToken }) => {
     const accessTokenJWT = createJWT({ payload: { user } })
