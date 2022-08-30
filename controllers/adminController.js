fcb38b221060b5fd61f9c902b3bc7efad16a20c8
Raunak <raunakgurud2002@gmail.com>
2022-08-30 20:15:48 +0530

    30-8-22

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,25 +2,49 @@ const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
 
 const getAllUsers = (req, res) => {
-
     User.find({}, function (err, result) {
         if (err) {
             console.log(err)
         }
         res.json(result)
     })
-
 }
 
 const getUserById = async (req, res) => {
     const { params: { id: userId, } } = req
     const user = await User.findOne({ _id: userId })
+
+    if (!user) {
+        res.status(StatusCodes.BAD_REQUEST).json({ message: "No user with this id" })
+    }
+    res.status(StatusCodes.OK).json(user)
+}
+
+const updateUserById = async (req, res) => {
+
+    const { body: { role }, params: { id: userId, } } = req
+    const user = await User.findOne({ _id: userId })
+
+    res.status(StatusCodes.OK).json(user)
+}
+
+const deleteUserById = async (req, res) => {
+    const { params: { id: userId, } } = req
+    const user = await User.findByIdAndDelete({ _id: userId })
+
+    if (!user) {
+        res.status(StatusCodes.BAD_REQUEST).json({ message: "No user with this id" })
+    }
     res.status(StatusCodes.OK).json(user)
 }
 
 
 
+
+
 module.exports = {
     getAllUsers,
-    getUserById
+    getUserById,
+    updateUserById,
+    deleteUserById,
 } 
