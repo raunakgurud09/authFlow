f49338512dceaddac328815729d66591b5ee3c10
Raunak <raunakgurud2002@gmail.com>
2022-08-30 20:08:31 +0530

    making code clean

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,9 +3,22 @@ const Router = require("express").Router();
 const { getAllUsers, getUserById } = require("../controllers/adminController");
 const { authorizePermissions, authUser } = require("../middlewares/authentication");
 
+const { test } = require("../controllers/userController");
 
 
-Router.route("/users").get(authUser, authorizePermissions('admin'), getAllUsers)
-Router.route("/user/:id").get(authUser, authorizePermissions('admin'), getUserById)
+
+Router
+    .route("/users")
+    .get(authUser, authorizePermissions('admin'), getAllUsers)
+
+Router
+    .route("/user/:id")
+    .get(authUser, authorizePermissions('admin'), getUserById)
+    .patch()
+    .delete()
+
+
+Router.get("/test", authUser, authorizePermissions('admin', 'superadmin'), test)
+
 
 module.exports = Router;
