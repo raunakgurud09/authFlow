804a9cdf2c283d74fccd83520b897cb423a346a8
raunak gurud <raunakgurud2121@gmail.com>
2022-09-25 19:16:30 +0530

    adding google autho

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
-
+require('./utils/google.auth')
 //  express
 const express = require("express");
 const app = express();
 
 //  rest of the packages
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
+const passport = require('passport')
 
 //  import configs
 const connectDB = require("./configs/DbConnect");
@@ -18,21 +19,45 @@ const { cloudinary } = require("./configs/cloudinary");
 //  routersImport
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
-const adminRoute = require("./routes/adminRoutes")
+const adminRoute = require("./routes/adminRoutes");
 
 //  middleware
 app.use(express.json());
-app.use(cookieParser(process.env.JWT_SECRET))
+app.use(cookieParser(process.env.JWT_SECRET));
 app.use(fileUpload({ useTempFiles: true }));
 
-app.get('/',(req,res)=>{
-  res.send('<a href="/auth/google">Google</a>')
-})
+app.get("/", (req, res) => {
+  res.send('<a href="/auth/google">Google</a>');
+});
+
+app.get("/google/callback", (req, res) => {
+  res.send("<p>redirect</p>");
+});
+app.get("/bad", (req, res) => {
+  res.send("<p>bad</p>");
+});
+app.get("/good", (req, res) => {
+  res.send("<p>good</p>");
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email","profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {successRedirect:'/good', failureRedirect: "/bad" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/good");
+  }
+);
 
 //  routes config
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/admin", adminRoute)
+app.use("/api/v1/admin", adminRoute);
 
 //  port
 const start = async () => {
---
+++ utils/google.auth.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+const passport = require("passport");
+const User = require("../models/User");
+const JwtStrategy = require("passport-jwt").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/google/callback",
+    },
+    async function (accessToken, refreshToken, profile, cb) {
+      await User.findOrCreate(
+        { googleId: profile.id },
+        { first_name: profile.displayName, email: profile._json.email },
+        function (err, user) {
+          return cb(err, user);
+        }
+      );
+    }
+  )
+);
+// JWT
+const cookieExtractor = (req) => {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies["access_token"];
+  }
+
+  return token;
+};
+
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: cookieExtractor,
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    (payload, done) => {
+      User.findById(payload.sub, (err, user) => {
+        if (err) {
+          return done(err, false);
+        }
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      });
+    }
+  )
+);
+
+// CONFIGURE AUTHENTICATED SESSION PERSISTENCE
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
