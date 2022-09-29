55053d5ad121dc2ed445faece64c5b41075d04b0
raunak gurud <raunakgurud2121@gmail.com>
2022-09-29 23:44:44 +0530

    adding:security packages

--- server.js
+++ server.js
@@ -5,32 +5,47 @@ const express = require("express");
 const app = express();
 
 //  rest of the packages
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mongoSanitizer = require("express-mongo-sanitize");
 const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 
 //  import configs
 const connectDB = require("./configs/DbConnect");
 const dotenvConfig = require("./configs/dotenv.config");
-const { cloudinary } = require("./configs/cloudinary");
+
 //  database
 
 //  routersImport
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
-const adminRoute = require("./routes/adminRoutes")
+const adminRoute = require("./routes/adminRoutes");
 
 //  middleware
 app.use(express.json());
-app.use(cookieParser(process.env.JWT_SECRET))
+app.use(cookieParser(process.env.JWT_SECRET));
 app.use(fileUpload({ useTempFiles: true }));
+app.use(cors({ origin: "*" }));
+app.use(mongoSanitizer());
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  })
+);
+app.use(helmet());
+app.use(xss());
 
-app.use(express.static("./public"))
+app.use(express.static("./public"));
 
 //  routes config
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/admin", adminRoute)
+app.use("/api/v1/admin", adminRoute);
 
 //  port
 const start = async () => {
