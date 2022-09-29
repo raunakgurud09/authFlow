128d51b81ba4ca477d8ae71c7ee78c43c0368756
raunak gurud <raunakgurud2121@gmail.com>
2022-09-29 21:59:56 +0530

    cleaning:development.env

--- configs/URL.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-
-module.export = {
-  MONGO_URI: `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.fl4iz.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
-};
-
